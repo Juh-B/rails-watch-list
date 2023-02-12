94df138e51f9f818c92a1b81e95091edf75b236b
Juliana Bonardi <julicbonardi@hotmail.com>
2023-02-12 18:41:35 -0300

    updating seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -28,7 +28,7 @@ print 'Creating top movies...'
 top_movies['results'].each do |movie|
   title = movie['title']
   overview = movie['overview']
-  poster_url = "https://image.tmdb.org/t/p/w450#{movie['poster_path']}"
+  poster_url = "https://image.tmdb.org/t/p/w300#{movie['poster_path']}"
   rating = movie['vote_average'].to_f
 
   Movie.create!(title: title, overview: overview, poster_url: poster_url, rating: rating)
@@ -36,12 +36,11 @@ end
 puts 'Created!'
 
 # CREATING MOVIES - SEEDS
-# print 'Creating more movies...'
-# Movie.create!(title: 'Wonder Woman 1984', overview: 'Wonder Woman comes into conflict with the Soviet Union during the Cold War in the 1980s', poster_url: "https://image.tmdb.org/t/p/original/8UlWHLMpgZm9bx6QYh0NFoq67TZ.jpg", rating: 6.9)
-# Movie.create!(title: 'The Shawshank Redemption', overview: 'Framed in the 1940s for double murder, upstanding banker Andy Dufresne begins a new life at the Shawshank prison', poster_url: "https://image.tmdb.org/t/p/original/q6y0Go1tsGEsmtFryDOJo3dEmqu.jpg", rating: 8.7)
-# Movie.create!(title: 'Titanic', overview: '101-year-old Rose DeWitt Bukater tells the story of her life aboard the Titanic.', poster_url: "https://image.tmdb.org/t/p/original/9xjZS2rlVxm8SFx8kPC3aIGCOYQ.jpg", rating: 7.9)
-# Movie.create!(title: "Ocean's Eight", overview: 'Debbie Ocean, a criminal mastermind, gathers a crew of female thieves to pull off the heist of the century.', poster_url: "https://image.tmdb.org/t/p/original/MvYpKlpFukTivnlBhizGbkAe3v.jpg", rating: 7.0)
-# puts 'Created!'
+print 'Creating more movies...'
+Movie.create!(title: 'Wonder Woman 1984', overview: 'Wonder Woman comes into conflict with the Soviet Union during the Cold War in the 1980s', poster_url: "https://image.tmdb.org/t/p/w300/8UlWHLMpgZm9bx6QYh0NFoq67TZ.jpg", rating: 6.9)
+Movie.create!(title: 'Titanic', overview: '101-year-old Rose DeWitt Bukater tells the story of her life aboard the Titanic.', poster_url: "https://image.tmdb.org/t/p/w300/9xjZS2rlVxm8SFx8kPC3aIGCOYQ.jpg", rating: 7.9)
+Movie.create!(title: "Ocean's Eight", overview: 'Debbie Ocean, a criminal mastermind, gathers a crew of female thieves to pull off the heist of the century.', poster_url: "https://image.tmdb.org/t/p/w300/MvYpKlpFukTivnlBhizGbkAe3v.jpg", rating: 7.0)
+puts 'Created!'
 
 # CREATING THE FIRST LIST
 print 'Creating a list...'
